3f8ba09bb50e9d19f2b3865f5fc2d6434faa8d9f
ilhamfzz <ilhamfz117@gmail.com>
2023-08-09 05:24:52 +0700

    Add project files.

---
+++ FakhriWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FakhriWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
